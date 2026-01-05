49ce8219b3cc896a5b5812c52b00c8aec5e41e28
mausamacharyaa <mausamsharmaa07@gmail.com>
2026-01-05 16:05:34 +0545

    committed some learned dart programs

--- PR1/main.dart
+++ PR1/main.dart
@@ -1,3 +1,28 @@
 void main() {
   print("mausam donna");
 }
+/* 
+
+###
+variables
+
+int(1,2,...)
+double(2.13)
+string("mausam ")
+bool (istrue or isfalse)
+dynamic(1,2.1,mausam ,etc..)
+
+
+###
+var --> can be changable
+final--> somthin in it can be changed , but it ant be changed itself like time.. known after execution
+const--> cannot be changed in any sircumstances and its is fixed values already known like pi=3.14...
+
+
+###
+
+
+
+
+*/
+
